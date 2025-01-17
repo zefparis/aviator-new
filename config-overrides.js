befcf1b00a02016b48ea272c2a5dc45f1ff34ea0
cashblaze127 <cashblaze129@gmail.com>
2025-01-17 11:09:30 +0900

    hotfix: updated process module

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,30 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 
-module.exports = function override(config) {
-	// config.output.path += '/../../deploy/web';
-	const fallback = config.resolve.fallback || {"fs": false};
-	Object.assign(fallback, {
-		crypto: require.resolve("crypto-browserify"),
-		stream: require.resolve("stream-browserify"),
-		// assert: require.resolve("assert"),
-		path: require.resolve("path-browserify"),
-		http: require.resolve("stream-http"),
-		https: require.resolve("https-browserify"),
-		os: require.resolve("os-browserify"),
-		url: require.resolve("url"),
-		assert: require.resolve("browserify-assets")
-	});
-	config.resolve.fallback = fallback;
-	config.plugins = (config.plugins || []).concat([
-		new webpack.ProvidePlugin({
-			process: "process/browser",
-			Buffer: ["buffer", "Buffer"],
-		}),
-	]);
-	return config;
+module.exports = function override(config, env) {
+    // Add fallback for node modules
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
+        process: require.resolve('process/browser'),
+        zlib: require.resolve('browserify-zlib'),
+        stream: require.resolve('stream-browserify'),
+        util: require.resolve('util/'),
+        buffer: require.resolve('buffer/'),
+        asset: require.resolve('assert/'),
+        crypto: require.resolve('crypto-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        path: require.resolve('path-browserify'),
+        url: require.resolve('url/')
+    };
+
+    // Add ProvidePlugin to make node modules available globally
+    config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    ]);
+
+    return config;
 };
